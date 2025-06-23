6b5d045644649c11d985385de12aeb58673b54f4
Sachal Mohar <sachalmoohar@gmail.com>
2025-06-23 13:30:13 -0700

    Updated EmailGeneratorService and Controller with WebClient integration

--- src/main/java/com/email/writer/app/EmailGeneratorController.java
+++ src/main/java/com/email/writer/app/EmailGeneratorController.java
@@ -1,19 +1,26 @@
 package com.email.writer.app;
 
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+  
 
 @RestController
 @RequestMapping("/api/email")
+@AllArgsConstructor
 public class EmailGeneratorController {
-    
-    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest EmailRequest) {
-        return ResponseEntity.ok("");
+
+    private final EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
     }
     
 }
